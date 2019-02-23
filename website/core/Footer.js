a38cba1a856b7f7394358c7aab465e0172a58f8f
Bhanu Teja <pbteja1998@gmail.com>
2019-02-23 14:21:08 +0530

    initial commit

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,39 +37,39 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('about.html', this.props.language)}>
+              Getting Started
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+            <a href={this.docUrl('about.html', this.props.language)}>
+              Guides
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
+            <a href={this.docUrl('about.html', this.props.language)}>
+              API Reference
             </a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
-            <a
-              href="http://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
-            </a>
+            {/*<a href={this.pageUrl('users.html', this.props.language)}>*/}
+              {/*User Showcase*/}
+            {/*</a>*/}
+            {/*<a*/}
+              {/*href="http://stackoverflow.com/questions/tagged/"*/}
+              {/*target="_blank"*/}
+              {/*rel="noreferrer noopener">*/}
+              {/*Stack Overflow*/}
+            {/*</a>*/}
+            <a href="https://discord.gg/qEA8vxd">Project Chat</a>
+            {/*<a*/}
+              {/*href="https://twitter.com/"*/}
+              {/*target="_blank"*/}
+              {/*rel="noreferrer noopener">*/}
+              {/*Twitter*/}
+            {/*</a>*/}
           </div>
           <div>
             <h5>More</h5>
             <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
+            <a href="https://github.com/pbteja1998/OpenRank">GitHub</a>
             <a
               className="github-button"
               href={this.props.config.repoUrl}
@@ -83,18 +83,18 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
-          href="https://code.facebook.com/projects/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
+        {/*<a*/}
+          {/*href="https://code.facebook.com/projects/"*/}
+          {/*target="_blank"*/}
+          {/*rel="noreferrer noopener"*/}
+          {/*className="fbOpenSource">*/}
+          {/*<img*/}
+            {/*src={`${this.props.config.baseUrl}img/oss_logo.png`}*/}
+            {/*alt="Facebook Open Source"*/}
+            {/*width="170"*/}
+            {/*height="45"*/}
+          {/*/>*/}
+        {/*</a>*/}
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );

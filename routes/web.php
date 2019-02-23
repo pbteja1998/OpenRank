d3203caf8a4a7b284c25939a01decd9663a871b1
Bhanu Teja <pbteja1998@gmail.com>
2019-02-24 00:09:06 +0530

    add frontend

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::group(['prefix' => 'admin'], function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+/* Don't write anything below this */
+Route::get('/{any}', 'SpaController@index')->where('any', '.*');

8fd53352b3ae0606015babb418453c4ad32e0ddb
Bhanu Teja <pbteja1998@gmail.com>
2019-02-24 00:17:41 +0530

    nit

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -11,6 +11,6 @@ module.exports = {
     // modify the location of the generated HTML file.
     // make sure to do this only in production.
     indexPath: process.env.NODE_ENV === 'production'
-        ? 'index.html'
+        ? '../resources/views/index.blade.php'
         : 'index.html'
 };
